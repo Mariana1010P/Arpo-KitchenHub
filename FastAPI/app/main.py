5e070ce0f5e1b73e459d092e9f859bd616d1fad9
Mariana1010P <portela.mariana.6654@eam.edu.co>
2024-10-06 19:34:05 -0500

    improvements in main.py

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,1 +1,11 @@
-#Code aqui
+"""Main module."""
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.get("/", include_in_schema=False)
+async def read_root():
+    """Redirect to documentation."""
+    return RedirectResponse(url="/docs")
